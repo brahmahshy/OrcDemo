221f42e0fbe6600ae23ec2df5c952bad09b4c4d4
875101222 <875101222@qq.com>
2025-05-12 22:03:18 +0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.easyacg.orcdemo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.easyacg.orcdemo to javafx.fxml;
+    exports com.easyacg.orcdemo;
+}
